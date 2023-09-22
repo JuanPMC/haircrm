318b5acff9700530151b254a57ea495250c4ee82
Juan <juan.technova@gmail.com>
2023-09-22 09:40:28 -0500

    deleted make migrations

--- build_fresh.sh
+++ build_fresh.sh
@@ -3,7 +3,7 @@
 # build_files.sh
 pip install -r requirements.txt
 
-python3 manage.py makemigrations
+#python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
 
