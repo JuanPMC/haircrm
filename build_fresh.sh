44eb4476cd8919c0c6db4e3eb4713dcaadc7c800
Juan <juan.technova@gmail.com>
2023-09-22 08:09:08 -0500

    Added testigo

--- build_fresh.sh
+++ build_fresh.sh
@@ -4,3 +4,5 @@ pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
+
+echo "hola cara cola" > ./authapp/templates/welcome.html
